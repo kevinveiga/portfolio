eb4115fb7794948b76919b9aebc200c21d50f71d
Kevin M. Veiga <kevin.veiga@gmail.com>
2023-07-01 11:56:17 -0300

    up

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,23 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = function (api) {
-    api.cache(true);
+  api.cache(true);
 
-    return {
-        plugins: [
-            [
-                'styled-components',
-                {
-                    displayName: dev,
-                    fileName: dev,
-                    minify: true,
-                    preprocess: false,
-                    pure: false,
-                    ssr: true,
-                    transpileTemplateLiterals: true
-                }
-            ]
-        ],
-        presets: ['next/babel']
-    };
+  return {
+    plugins: [
+      [
+        'styled-components',
+        {
+          displayName: dev,
+          fileName: dev,
+          minify: true,
+          preprocess: false,
+          pure: false,
+          ssr: true,
+          transpileTemplateLiterals: true
+        }
+      ]
+    ],
+    presets: ['next/babel']
+  };
 };
