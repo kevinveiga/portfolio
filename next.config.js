950ea2040ac431d6fe87fcf3095eac82552119f5
Kevin M. Veiga <kevin.veiga@gmail.com>
2023-07-01 12:12:12 -0300

    up

--- next.config.js
+++ next.config.js
@@ -12,14 +12,5 @@ module.exports = withBundleAnalyzer({
     formats: ['image/avif', 'image/webp']
   },
   output: 'export',
-  reactStrictMode: true,
-  webpack(config, { dev, isServer }) {
-    const newConfig = config;
-
-    if (!dev) {
-      newConfig.devtool = isServer ? false : 'eval-source-map';
-    }
-
-    return newConfig;
-  }
+  reactStrictMode: true
 });
